1a39a0dd09ec5a4928378b3e9d45aa4fef4e2154
Croydon <git@cr0ydon.com>
2026-03-04 11:05:15 +0100

    Delete Chocolatey

--- chocolatey/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-# download and unpack a zip file
-Install-ChocolateyZipPackage `
-  -PackageName "winflexbison3" `
-  -Url "https://github.com/lexxmark/winflexbison/releases/download/v2.5.25/win_flex_bison-2.5.25.zip" `
-  -Checksum "8D324B62BE33604B2C45AD1DD34AB93D722534448F55A16CA7292DE32B6AC135" `
-  -ChecksumType "sha256" `
-  -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
